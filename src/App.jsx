84af19683e092836828171476d1564610babdd5b
AKASH KAPOOR <66661806+akashkapoor0001@users.noreply.github.com>
2024-10-21 19:01:48 +0000

    Updated the Image in the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Akash Kapoor",
+  title: "Mern Stack & C++ Developer",
+  email: "akash@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
